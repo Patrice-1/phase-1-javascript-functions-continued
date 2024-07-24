8cea22cd603484686ad744cb8f7fab874d01d161
Patrick <njorogepatrick739@gmail.com>
2024-07-24 12:16:06 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity= "go to the office"){
+    return `This Monday, I will ${activity}.`;
+
+}
+mondayWork("work from home");
+
+function wrapAdjective(flair="*"){
+  
+  return function (adjective= "a hard worker"){
+    return `You are ${flair}${adjective}${flair}!`;
+  }
+    
+}
+wrapAdjective ("||")("a dedicated programmer");
